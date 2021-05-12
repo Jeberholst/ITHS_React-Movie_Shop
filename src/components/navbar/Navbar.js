0b80a0161bb6fbe6f03e6d29a375fb18fe6cd9f5
Wstld <52714297+Wstld@users.noreply.github.com>
2021-05-12 20:00:57 +0200

    Basic menu added

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,10 +1,13 @@
 import './Navbar.css';
 import dummyLogo from '../../img/Logo/dummy_ic.jpg'
 
-const Navbar= () => {
+
+let menuOpen = true
+
+const Bar = () => {
   return (
     <div className="navbar__container">
-     <img src={dummyLogo} alt="" className="navbar__container_logo" />
+     <img src={dummyLogo} alt="" className="navbar_logo" />
      <div className="searchbar__container">
          <input type="text" className="searchbar__container_search-field" />
          <i className="fas fa-search searchbar__container_search-field_logo"></i>
@@ -15,6 +18,55 @@ const Navbar= () => {
     
 
     </div>
+  );
+} 
+
+let Menu = () => {
+  return(
+    <div className="menu__overlay">
+    
+    {/*top bar*/}
+      <div className="navbar__container">
+     <img src={dummyLogo} alt="" className="navbar_logo" />
+     <div className="navbar_company_title">
+         COMPANY NAME
+     </div>
+     <i className="fas fa-times"></i>
+    </div>
+
+    {/*menu*/}
+      <div className="menu__container">
+        <div className="menu_title_cont">
+          <h2 className="menu__title">Movies</h2>
+        </div>
+        <div className="menu__categories_cont">
+          <div className="categories_left">
+            <div className="categories_item">Action</div>
+            <div className="categories_item">Comedy</div>
+            <div className="categories_item">Drama</div>
+            <div className="categories_item">Romance</div>
+          </div>
+          <dic className="categories_right">
+            <div className="categories_item">Fantasy</div>
+            <div className="categories_item">Horror</div>
+            <div className="categories_item">Mystery</div>
+            <div className="categories_item">Thriller</div>
+          </dic>
+        </div>
+        <div className="menu__bottom_cont">
+
+        </div>
+      </div>
+    </div>
+  );
+}
+
+
+const Navbar= () => {
+  return (
+    <>
+    {menuOpen? <Menu></Menu> : <Bar></Bar>}
+    </>
   );
 }
 
