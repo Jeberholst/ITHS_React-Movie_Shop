b34ccf3a8a497b706b8569005a5848be20f04bda
Aneta Szmidt <anetaszmidt@Anetas-MacBook-Air.local>
2021-05-12 10:01:09 +0200

    Routing with pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import  {
+    Route,
+    Switch,
+    Link,
+    BrowserRouter as Router
+} from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+import CommentPage from "./pages/CommentPage";
+import LoginPage from "./pages/LoginPage";
+import ShoppingCartPage from "./pages/ShoppingCartPage";
+import SearchPage from "./pages/SearchPage";
+import ProfilePage from "./pages/ProfilePage";
+import GenrePage from "./pages/GenrePage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Movie Shop
-        </a>
-      </header>
-    </div>
+      <div className="App">
+
+        <Router  basename={`${process.env.PUBLIC_URL}/`}>
+          <ul style={{display: 'flex', flexDirection: 'column',  textAlign: "start" }}>
+              <li><Link to="/">LandingPage</Link></li>
+              <li><Link to="/genre">genre</Link></li>
+              <li><Link to="/comment">comment</Link></li>
+              <li><Link to="/login">login</Link></li>
+              <li><Link to="/profile">profile</Link></li>
+              <li><Link to="/search">search</Link></li>
+              <li><Link to="/shopping-cart">shopping-cart</Link></li>
+
+          </ul>
+          <div>
+            <Switch>
+              <Route exact path="/" component={LandingPage} />
+              <Route exact path="/genre" component={GenrePage} />
+              <Route exact path="/comment" component={CommentPage} />
+              <Route exact path="/login" component={LoginPage} />
+              <Route exact path="/profile" component={ProfilePage} />
+              <Route exact path="/search" component={SearchPage} />
+              <Route exact path="/shopping-cart" component={ShoppingCartPage} />
+
+            </Switch>
+          </div>
+        </Router>
+      </div>
   );
 }
 
---
+++ src/pages/LandingPage.js
@@ -0,0 +1,8 @@
+const LandingPage = () => {
+
+    return <p>LandingPage</p>
+
+
+}
+
+export default LandingPage
