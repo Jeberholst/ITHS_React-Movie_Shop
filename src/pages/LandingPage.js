9d178bfa919f979b66a044de24aa45b30bac8875
Aneta Szmidt <anetaszmidt@Anetas-MacBook-Air.local>
2021-05-13 13:04:32 +0200

    Marged Routing with Master

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,6 +1,24 @@
+import MockDataHolder from "../mockData/MockDataHolder";
+import React from "react";
+import {getSearchedTitlePaged, getTitleFull} from "../mockData/mock-data-fetcher";
+
 const LandingPage = () => {
 
-    return <p>LandingPage</p>
+
+    function fetchMockData(){
+        getTitleFull()
+        getSearchedTitlePaged()
+    }
+
+    return(
+
+    <div className="App-Content">
+        <p>LandingPage</p>
+        <h1 style={{color: '#388e3c'}}>Content-container--</h1>
+        {fetchMockData()}
+        <MockDataHolder/>
+    </div>
+    )
 
 
 }
