31cd67115a923174e91695ece5425bd3f7ac1fb7
Admin <J_eberholst@hotmail.com>
2021-05-13 12:53:22 +0200

    Revert "Merge branch 'master' of https://github.com/Jeberholst/iths-react-movie-shop into navbar"
    This reverts commit 9bd1148ffea476a86f9e4d05d0c20b5543fcdb67, reversing
    changes made to f716a631f7258702228a47657e5164b3da3a272a.

--- src/mockData/MockMoviePaged.jsx
+++ src/mockData/MockMoviePaged.jsx
@@ -1,33 +1,14 @@
-import { makeStyles } from '@material-ui/core';
 import React from 'react'
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
-    flexDirection: 'column',
-    width: '100%',
-    height: '100%',
-    alignItems: 'left',
-    textAlign: 'left',
-    alignContent: 'left',
-    background: '#1f1f1f',
-  },
-  text: {
-    margin: 15,
-    color: '#f3f3f3',
-  },
-}));
-
 const MockMoviePaged = ({item}) => {
-  const classes = useStyles();
 
   return(
     <React.Fragment>
-       <div className={classes.root}>
-        <div className={classes.text}>
+       <div style={{display: 'flex', flexDirection: 'row', width: '50%', margin: 5}}>
+        <p>
           {item.title}	
           {item.year}	
-        </div>
+        </p>
       </div>
     </React.Fragment>
   );
--- src/mockData/MockMovieSingle.jsx
+++ src/mockData/MockMovieSingle.jsx
@@ -1,56 +1,38 @@
-import { makeStyles } from '@material-ui/core';
 import React from 'react'
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
-    flexDirection: 'column',
-    width: '100%',
-    height: '100%',
-    alignItems: 'left',
-    textAlign: 'left',
-    alignContent: 'left',
-    background: '#1f1f1f',
-  },
-  text: {
-    margin: 15,
-    wordBreak: 'break-word'
-  }
-}));
-
 const MockMovieSingle = ({item}) => {
-    const classes = useStyles();
 
     return(
       <React.Fragment>
-          <div className={classes.root}>
-            <div className={classes.text}>
-              {item.title}	
-              {item.year}	
-              {item.rated}
-              {item.released}	
-              {item.runtime}	
-              {item.genre}	
-              {item.director}	
-              {item.writer}	
-              {item.actors}	
-              {item.plot}	
-              {item.language}	
-              {item.country}	
-              {item.awards}
-              {item.poster}	
-              {/* {item.ratings} */}
-              {item.metascore}	
-              {item.imdbrating}	
-              {item.imdbvotes}
-              {item.imdbid}	
-              {item.type}	
-              {item.dvd}	
-              {item.boxoffice}	
-              {item.production}	
-              {item.website}	
-              {item.response}
-          </div>
+          <div style={{display: 'flex', flexDirection: 'row', width: '50%'}}>
+            <p>
+            {item.title}	
+            {item.year}	
+            {item.rated}
+            {item.released}	
+            {item.runtime}	
+            {item.genre}	
+            {item.director}	
+            {item.writer}	
+            {item.actors}	
+            {item.plot}	
+            {item.language}	
+            {item.country}	
+            {item.awards}
+            {item.poster}	
+            {/* {item.ratings} */}
+            {item.metascore}	
+            {item.imdbrating}	
+            {item.imdbvotes}
+            {item.imdbid}	
+            {item.type}	
+            {item.dvd}	
+            {item.boxoffice}	
+            {item.production}	
+            {item.website}	
+            {item.response}
+          </p>
+  
         </div>
       </React.Fragment>
     );
