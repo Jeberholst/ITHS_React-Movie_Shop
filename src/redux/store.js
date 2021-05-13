8cb84559a1bc2080f5d2f61104c9be9f8528bc9e
Wstld <52714297+Wstld@users.noreply.github.com>
2021-05-13 09:03:00 +0200

    Header (#4)
    * Header done
    * Navbar complete and, fontawsome added to index.html, app.css, app.js and index.css cleaned
    * Basic menu added
    * Very basic menu added

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { rootReducer } from './features/rootReducer';
 
+
 export default configureStore({
-  reducer : rootReducer,
+  reducer : rootReducer
 })
