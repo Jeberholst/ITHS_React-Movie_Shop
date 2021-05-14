eb9fa3db27eea18a16adadf04999b2d26d5f7eb2
Wstld <52714297+Wstld@users.noreply.github.com>
2021-05-14 14:12:42 +0200

    landpage Main slider done.

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,6 +1,7 @@
 import MockDataHolder from "../mockData/MockDataHolder";
 import React from "react";
-import {getSearchedTitlePaged, getTitleFull} from "../mockData/mock-data-fetcher";
+import {getSearchedTitlePaged, getTitleFull} from "../mockData/mock-data-fetcher.js";
+import MainSlider from "../components/slider/MainSlider.js";
 
 const LandingPage = () => {
 
@@ -9,13 +10,17 @@ const LandingPage = () => {
         getTitleFull()
         getSearchedTitlePaged()
     }
+    const images = [
+        'https://images.unsplash.com/photo-1449034446853-66c86144b0ad?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80',
+        'https://images.unsplash.com/photo-1470341223622-1019832be824?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2288&q=80',
+        'https://images.unsplash.com/photo-1448630360428-65456885c650?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2094&q=80',
+        'https://images.unsplash.com/photo-1534161308652-fdfcf10f62c4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2174&q=80'
+      ]
 
     return(
 
     <div className="App-Content">
-        <p>LandingPage</p>
-        {fetchMockData()}
-        <MockDataHolder/>
+        <MainSlider imgList = {images} ></MainSlider>
     </div>
     )
 
--- src/redux/features/rootReducer.js
+++ src/redux/features/rootReducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import { reducer as shoppingCartReducer } from './shoppingCart'
-import navbarReducer from './navbarSlice' 
+import navbarReducer from './navbarSlice'
+import mainSliderReducer from './mainSliderSlice' 
 
 const rootReducer = combineReducers({
     shoppingCart: shoppingCartReducer,
     navbar : navbarReducer,
+    mainslider : mainSliderReducer,
 })
 
 export { rootReducer };
