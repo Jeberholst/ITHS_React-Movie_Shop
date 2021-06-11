05042be0054722eca2c081bb030f9a8c273c4df5
Admin <J_eberholst@hotmail.com>
2021-06-11 08:15:11 +0200

    BillingInfo get

--- src/components/shopping-cart/CartBillingInfo.jsx
+++ src/components/shopping-cart/CartBillingInfo.jsx
@@ -1,7 +1,7 @@
 import { makeStyles } from "@material-ui/core";
 import React, { useEffect, useState } from "react";
 import authService from "../../util/auth-service";
-import { userBillingInfo } from "./../../mockData/mock-data-fetcher";
+import { fsDB as db } from '../../util/firebase';
 
 const cartTotalStyle = makeStyles((theme) => ({
   root: {
@@ -35,12 +35,56 @@ const cartTotalStyle = makeStyles((theme) => ({
   },
 }));
 
-// + ' ' + userBillingInfo.lastName}/>
+const intialUserInfo = {
+      address: null,
+      country: null,
+      county: null,
+      email: null,
+      firstname: null,
+      lastname: null,
+      postalCode: null,
+}
 
 const CartBillingInfo = () => {
   const classes = cartTotalStyle();
   const [user] = useState(authService.getCurrentUser());
-  //console.log(user)
+  const [userInfo, setUserInfo] = useState(intialUserInfo);
+  console.log('USER', user.uid)
+
+  const fetchBillingInfo = () => {
+
+      if(authService.getCurrentUser() !== null){
+
+        db.collection("users").doc(`${user.uid}`)
+            .onSnapshot((doc) => {
+                
+                if (doc.exists) {
+                    
+                    const data = doc.data()
+
+                    setUserInfo({
+                      address: data.address ? data.address : null,
+                      country: data.country ? data.country : null,
+                      county: data.county ? data.county : null,
+                      email: data.email ? data.email : null,
+                      firstname: data.firstname ? data.firstname : null,
+                      lastname: data.lastname ? data.lastname : null,
+                      postalCode: data.postalCode ? data.postalCode : null,
+                    })
+
+                } 
+
+        }, (error) => {
+            console.log("Error getting document, userinfo:", error);
+        });
+
+      }
+  }
+
+  useEffect(() => {
+      fetchBillingInfo()
+  }, [])
+
 
   return user ? (
     <React.Fragment>
@@ -51,16 +95,18 @@ const CartBillingInfo = () => {
           <Label text={"Name"} />
           <Label text={"Address"} />
           <Label text={"Postal Code"} />
-          <Label text={"County"} />
+          <Label text={"Email"} />
           <Label text={"Country"} />
+          <Label text={"County"} />
         </div>
 
         <div className={classes.right}>
           <Label text={user.displayName ? user.displayName : "-"} />
-          <Label text={user.county ? user.county : "-"} />
-          <Label text={user.address ? user.address : "-"} />
-          <Label text={user.postalCode ? user.postalCode : "-"} />
-          <Label text={user.country ? user.country : "-"} />
+          <Label text={userInfo.address ? userInfo.address : "-"} />
+          <Label text={userInfo.postalCode ? userInfo.postalCode : "-"} />
+          <Label text={userInfo.email ? userInfo.email : "-"} />
+          <Label text={userInfo.country ? userInfo.country : "-"} />
+          <Label text={userInfo.county ? userInfo.county : "-"} />
         </div>
 
       </div>
